7f8b9a18dd9d3a53d5402beca5490c870b6b6b99
Emily <meltzere@W-AMDEN-L-DES14.corp.pbwan.net>
2015-10-12 23:03:00 -0400

    style sheet for js

---
+++ Style.js
@@ -0,0 +1,79 @@
+// //variables
+// var firstName = Emily;
+// var lastName = Meltzer;
+// var fullName = firstName + lastName;
+
+// //debugging
+
+// //alert
+// alert ("Hello World!");
+
+// //comments
+
+// //this is a single line comment
+// /* this is a 
+// multi line comment */
+
+// // the console can be openeed by pressing comm (ctrl) + opt + J
+// /* You can also output to the console inside of a .js ﬁle using:
+// js console.log("My Content"); */
+
+// //data types
+
+// //string
+// "This is a string"
+
+// //number
+// 10, 20, .5, 2000
+
+// //boolean
+// true, false
+
+// //undefined (no value)
+
+// //arrays are used to hold a collection of data
+// //can hold letters or numbers or both
+// ["starbucks", "dunkin donuts", "seattles best", "folgers"];
+// //can also be stored in variable
+// var Name = ["Josie", "Ham"];
+// var Breed = ["Tabby", "Tuxedo"];
+// var myCats = ["Name", "Breed"];
+// //multi dimensional array
+
+// //array index always starts at 0
+
+// //testing
+// /*js can be put directly into the <scrip> </script> in html
+// or as an external .js file*/
+
+// //logic
+// //operator
+// // && (and)  (x < 10 && y > 1) is true
+// // || (or)   (x == 5 || y == 5) is false
+// // ! (not)   !(x == y) is true
+
+// //functions
+// function myFunction(p1, p2)
+// {
+// 	return p1 * p2;
+// }
+// // The function returns the product of p1 and p2
+
+// //3 fuctions
+
+var sisterName = "Ariel";
+var sisterNumber = "1";
+console.log("I have " + sisterNumber + " sister named " + sisterName);
+
+
+function sayHello()
+{
+	alert("Hi Al!");
+}
+
+function change()
+{
+	document.getElementById('el').innerHTML = "I've Come Undone!";
+}
+
+console.log("9" + "2");
